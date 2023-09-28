0cc31ed92af7284c8f03cc6ddeb5115c13493ae7
Aidan Carr <you@example.com>
2023-09-28 12:06:38 -0400

    Starting on shuffle function. Still a work in progess, testing off by one errors

--- Assignment 1/Sorting.cpp
+++ Assignment 1/Sorting.cpp
@@ -10,18 +10,37 @@
 //#include <cctype>
 using namespace std;
 
+
+//Swap 2 element positions given the array and both positions 
+void swap(string items[], int position1, int position2){
+    string temp = items[position1];
+    items[position1] = items[position2];
+    items[position2] = temp;
+}
+
+void shuffle(string items[]){
+    int length = 6;
+    for(int i = length-1; i > 0; i--){
+        int randomIndex;
+        randomIndex = rand() % i+1;
+        swap(items,i,randomIndex);
+    }
+
+}
+
+
 int main () {
 
     ifstream itemsFile;
     itemsFile.open("magicitems.txt");
-    string magicItems[666]; 
+    string magicItems[6]; 
 
     string item;
     string currentLine;
     if (itemsFile.is_open()){
         
         //assign each line to element in the array
-        for (int i = 0; i < 666; i++) {
+        for (int i = 0; i < 6; i++) {
             std::getline(itemsFile, currentLine);
             magicItems[i] = currentLine;
             std::cout << magicItems[i] << '\n';
@@ -32,10 +51,23 @@ int main () {
         std::cout << "Couldnt open file. \n";
     }
 
-    //
-    for (int i = 0; i < 666; i++) {
-        item = magicItems[i];
+    //Shuffle
+    shuffle(magicItems);
+
+    //Shuffle test print
+    for (int i = 0; i < 6; i++) {
+        std::cout << magicItems[i] << '\n';
     }
 
+    //Selection Sort
+
+
+    // Insertion Sort
+
+
+    //Merge Sort
+
+
+    //Quick Sort
 
 }
