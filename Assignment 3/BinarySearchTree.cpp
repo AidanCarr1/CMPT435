e42fddbe3077c3cb1d271eb137ea3f7a5db4778d
Aidan Carr <you@example.com>
2023-11-02 16:23:43 -0400

    Start Ass3. Create 2 C++ files

---
+++ Assignment 3/BinarySearchTree.cpp
@@ -0,0 +1,18 @@
+// Aidan Carr
+// November 2, 2023
+// Binary Search Tree
+
+//Compiled using g++
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <time.h>
+#include <cmath>
+using namespace std;
+
+
+//GLOBAL variables
+const int _NUM_OF_ITEMS = 666; //CONSTANT number of magic items
+const string _FILE_NAME = "magicitems.txt";
