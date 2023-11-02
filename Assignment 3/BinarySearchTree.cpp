b90af67f055912de6e1d41fcffa8bb7afcd3b7ec
Aidan Carr <you@example.com>
2023-11-02 17:33:25 -0400

    BST insert function rough draft

--- Assignment 3/BinarySearchTree.cpp
+++ Assignment 3/BinarySearchTree.cpp
@@ -43,6 +43,38 @@ public:
 };
 
 
+//Binary Search Tree class
+class BinarySearchTree{
+
+public:
+    Node* root;
+
+    //Constructor
+    BinarySearchTree(){
+        root = nullptr;
+    }
+};
+
+//populate magicItems[] with the item names from the txt file
+    //from Assignment 2
+void setMagicItemsArray(string items[]){ 
+    //Open the file
+    ifstream itemsFile;
+    itemsFile.open(_FILE_NAME);
+
+    string currentLine;
+    if (itemsFile.is_open()){
+
+        //assign each line to an element in the array
+        for (int i = 0; i < _NUM_OF_ITEMS; i++){
+            std::getline(itemsFile, currentLine);
+            items[i] = currentLine;
+        }
+    }
+    else {}
+}
+
+
 //Compare 2 strings in alphabetical order
 //return true is first string comes first, false if second string is first
 bool isLessThan(string first, string second){
@@ -65,4 +97,62 @@ bool isLessThan(string first, string second){
     //shorter string is less than larger
     //returns false if strings are equal
     return (length1 < length2)? true : false;
+}
+
+
+
+//insert a string into the BST
+void insert(BinarySearchTree tree, string name){
+    
+    //create a new node
+    Node* myNode = new Node();
+    myNode->itemName = name;
+
+    //these might not work: new node, pointers...
+    Node* trailing = nullptr;
+    Node* current = tree.root;
+
+    //find a spot for the new Node
+    while (current != nullptr){
+        trailing = current;
+        if (isLessThan(name, current->itemName)){
+            current = current->left;
+            std::cout << "L";
+        }
+        else {
+            current = current->right;
+            std::cout << "R";
+        }
+    }
+    std::cout << endl;
+
+    //put the new Node in the spot
+    myNode->parent = trailing;
+    if (trailing == nullptr){
+        tree.root = myNode;
+    }
+    else if (isLessThan(name, trailing->itemName)){
+        trailing->left = myNode;
+    }
+    else {
+        trailing->right = myNode;
+    }
+}
+
+
+//Main program!
+//Open file, put into array, pick random 42, search, and print comparisons
+int main () {
+
+    //put 666 magic items into magicItems array
+    string magicItems[_NUM_OF_ITEMS];
+    setMagicItemsArray(magicItems);
+
+    //create a Binary Search Tree
+    BinarySearchTree magicBST;
+
+    for (int i = 0; i < _NUM_OF_ITEMS; i++){
+        insert(magicBST, magicItems[i]);
+    }
+
 }
