8b50f7c8226c66fd1f17ee27441c5d863a881482
Aidan Carr <you@example.com>
2023-11-09 22:50:32 -0500

    start file processing for graphs file

--- Assignment 3/Graphs.cpp
+++ Assignment 3/Graphs.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 
 //GLOBAL variables
-const string _FILE_NAME = "graphs1.txt";
+const string _FILE_NAME = "graphs0.txt";
 const int _LINES_IN_FILE = 375; //number of lines in graphs1.txt
 
 
@@ -36,3 +36,63 @@ public:
         neighbors.push_back(newNeighbor);
     }
 };
+
+
+//Main Program!
+//Read graphs file and interpret: make graphs, vertices, edges
+int main() {
+
+    //Open the file
+    std::ifstream graphsFile;
+    graphsFile.open(_FILE_NAME);
+
+    string currentLine;
+    if (graphsFile.is_open()){
+
+        //go line by line in the file
+        while (graphsFile){
+
+            //get line as a string
+            std::getline(graphsFile, currentLine); 
+            //std::cout << currentLine << endl; //test line
+
+            //empty line, ignore line
+            if (currentLine.compare(0,1,"") == 0){
+                //std::cout << "empty line" << endl; //test line
+            }
+
+            //comment, ignore line
+            else if (currentLine.compare(0,2,"--") == 0){
+                //std::cout << "comment" << endl; //test line
+            }
+
+            //new graph
+            else if (currentLine.compare(0,3,"new") == 0){
+                //std::cout << "NEW graph" << endl;
+                //print the previous graph (if previous exists)
+                //delete graph
+            }
+
+            //new vertex
+            else if (currentLine.compare(4,6,"vertex") == 0){
+                int strLength = currentLine.length();
+                int vertexNum = stoi(currentLine.substr(11,11-strLength));
+                std::cout << vertexNum << endl; //test line
+
+                //create vertex object
+            }
+
+            //new edge
+            else if (currentLine.compare(4,4,"edge") == 0){
+
+            }
+
+            else {
+                std::cout << "ERROR!\n";
+                std::cout << currentLine << "\n" << std::endl;
+            }
+        }
+    }
+    else {}
+    graphsFile.close();
+}
