6bab37fd9526e5fbccea628425255942aff9c9f2
Aidan Carr <you@example.com>
2023-11-27 16:27:29 -0500

    Ass4. Add file reading to both c++ codes

--- Assignment 4/BellmanFord.cpp
+++ Assignment 4/BellmanFord.cpp
@@ -15,3 +15,42 @@ using namespace std;
 
 //GLOBAL variables
 const string _FILE_NAME = "graphs2.txt";
+
+//Main Program!
+//Read the file,
+
+int main(){
+
+    //open the file
+    std::ifstream graphsFile;
+    graphsFile.open(_FILE_NAME);
+    
+    //create vector
+    int fileLength = 0;
+    vector<string> fileCommands;
+
+    string currentLine;
+    if (graphsFile.is_open()){
+
+        //read the file into a vector
+        while (graphsFile){
+            //insert commands into vector
+            std::getline(graphsFile, currentLine); 
+            fileCommands.push_back(currentLine);
+            fileLength++;
+        }
+
+        //IO duplicates final line, delete it
+        fileCommands.pop_back(); 
+        //print out final graph
+        fileCommands.push_back("new graph");
+    }    
+    else {}
+    graphsFile.close();   
+
+    //* //test lines
+    for(int i = 0; i < fileCommands.size(); i++){
+        std::cout << fileCommands[i] << endl;
+    }
+    //*/
+}
--- Assignment 4/Greedy.cpp
+++ Assignment 4/Greedy.cpp
@@ -15,3 +15,41 @@ using namespace std;
 
 //GLOBAL variables
 const string _FILE_NAME = "spice.txt";
+
+
+//Main Program!
+//Read the file,
+
+int main(){
+
+    //open the file
+    std::ifstream spiceFile;
+    spiceFile.open(_FILE_NAME);
+    
+    //create vector
+    int fileLength = 0;
+    vector<string> fileCommands;
+
+    string currentLine;
+    if (spiceFile.is_open()){
+
+        //read the file into a vector
+        while (spiceFile){
+            //insert commands into vector
+            std::getline(spiceFile, currentLine); 
+            fileCommands.push_back(currentLine);
+            fileLength++;
+        }
+
+        //IO duplicates final line, delete it
+        fileCommands.pop_back(); 
+    }    
+    else {}
+    spiceFile.close();   
+
+    /* //test lines
+    for(int i = 0; i < fileCommands.size(); i++){
+        std::cout << fileCommands[i] << endl;
+    }
+    //*/
+}
