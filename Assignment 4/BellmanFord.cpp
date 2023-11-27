e89eeb70058960e1420529e9f60822c68b6747d4
Aidan Carr <you@example.com>
2023-11-27 00:21:11 -0500

    Start Ass4. Initialize all c++ and txt files.

---
+++ Assignment 4/BellmanFord.cpp
@@ -0,0 +1,17 @@
+// Aidan Carr
+// November 27, 2023
+// SSSP: Directed Graphs
+
+//Compiled using g++
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+//GLOBAL variables
+const string _FILE_NAME = "graphs2.txt";
---
+++ Assignment 4/Greedy.cpp
@@ -0,0 +1,17 @@
+// Aidan Carr
+// November 27, 2023
+// Greedy Algorithm: Spice Heist
+
+//Compiled using g++
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+//GLOBAL variables
+const string _FILE_NAME = "spice.txt";
