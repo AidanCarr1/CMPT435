f44bdb8684ec49b15c8a1bcd39e8563aa6e135f1
Aidan Carr <you@example.com>
2023-11-27 16:57:16 -0500

    Command interpreting spice.txt mostly complete

--- Assignment 4/Greedy.cpp
+++ Assignment 4/Greedy.cpp
@@ -1,6 +1,6 @@
 // Aidan Carr
 // November 27, 2023
-// Greedy Algorithm: Spice Heist
+// Greedy Algorithm: The Spice Heist
 
 //Compiled using g++
 
@@ -47,9 +47,62 @@ int main(){
     else {}
     spiceFile.close();   
 
-    /* //test lines
-    for(int i = 0; i < fileCommands.size(); i++){
-        std::cout << fileCommands[i] << endl;
+    vector<int> knapsackCapacities;
+
+
+    //INTERPRET ALL COMMANDS
+    for(int line = 0; line < fileLength; line ++){
+
+        //get line as a string, and find length
+        string currentLine = fileCommands[line]; 
+        int strLength = currentLine.length();
+
+        //empty line, ignore line
+        if (currentLine.compare(0,1,"") == 0){
+        }
+
+        //comment, ignore line
+        else if (currentLine.compare(0,2,"--") == 0){
+        }
+
+        //new Spice
+        else if (currentLine.compare(0,5,"spice") == 0){
+            
+            //get name
+            int equalsIndex = currentLine.find("=");
+            int colonIndex = currentLine.find(";");
+            string name = currentLine.substr(equalsIndex +2, colonIndex - equalsIndex-2);
+
+            //get total_price
+            currentLine = currentLine.substr(colonIndex +2, strLength - colonIndex-2);
+            equalsIndex = currentLine.find("=");
+            colonIndex = currentLine.find(";");
+            string strPrice = currentLine.substr(equalsIndex +2, colonIndex - equalsIndex-2);
+            float totalPrice = std::stof(strPrice);
+
+            //get qty
+            currentLine = currentLine.substr(colonIndex +2, strLength - colonIndex-2);
+            equalsIndex = currentLine.find("=");
+            colonIndex = currentLine.find(";");
+            string strQuantity = currentLine.substr(equalsIndex +2, colonIndex - equalsIndex-2);
+            int quantity = std::stof(strQuantity);
+
+            std::cout << "Name: " << name << ". Total Price: $" << totalPrice << ". Quantity: " << quantity << "." << endl; //test line
+            
+            //create spice object? using data
+
+        }
+
+        //new knapsack size
+        else if (currentLine.compare(0,4,"knap") == 0){
+            int equalsIndex = currentLine.find("=");
+            string strCapacity = currentLine.substr(equalsIndex +2, strLength - equalsIndex -3);
+            int capacity = std::stof(strCapacity);
+            //add capacity to vector of knapsacks
+            knapsackCapacities.push_back(capacity);
+
+        }
+            
     }
-    //*/
+            
 }
